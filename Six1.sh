d3aa982e8891a27cfa3bfa30d68e01b2c67aaad2
Sixmusic807725 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-13 22:50:31 +0700

    Six1.sh
    Coba aja dulu ×_×

---
+++ Six1.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#Apa Lu Mau Recode? Ini Buat Belajar Tolol
+#Dasar Anak Ngentod Yang Taunya Recode Aja:v
+#Rapi Kan Kodingan Gw Su?
+
+clear
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+bl='\33[36;1m' #BlueLight
+echo $bl""
+figlet -f standard Belajar
+echo "_____________________________________________________________"
+
+echo "Tools Kampank V1 On Termosss" | lolcat
+echo $g"Author : Cx12|©copyright|" 
+echo "Contact Admin : 085736891082" | lolcat
+echo $g"Recod3? Besok dapet kesialan se umur hidup"
+ echo "_____________________________________________________________"
+
+echo $bl"Untuk Melanjutkan Silahkan Login Dulu!!"
+echo ""
+
+sleep 3
+
+read -p "Username : " usr
+
+if [ $usr = "IvanId" ]
+then
+
+figlet -f standard Sukses! | lolcat
+
+else
+  figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh BelajarKoding.sh
+
+fi
+
+read -p "Password: " pwd
+
+if [ $pwd = "Mr.Revoers" ]
+then
+#!/bin/bash
+#Apa Lu Mau Recode? Ini Buat Belajar Tolol
+#Dasar Anak Ngentod Yang Taunya Recode Aja:v
+#Rapi Kan Kodingan Gw Su?
+
+clear
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+bl='\33[36;1m' #BlueLight
+echo $bl""
+figlet -f standard Belajar
+echo "_____________________________________________________________"
+
+echo "Tools Cx12 V1 On Termosss" | lolcat
+echo $g"Author : Cx12|©copyright|" 
+echo "Contact Admin : 081227443846" | lolcat
+echo $g"Recod3? Besok dapat kesialan se umur hidup"
+ echo "_____________________________________________________________"
+
+echo $bl" Login Dulu!!"
+echo ""
+
+sleep 3
+
+read -p "Username : " usr
+
+if [ $usr = "cihuhay12" ]
+then
+
+figlet -f standard Sukses! | lolcat
+
+else
+  figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh BelajarKoding.sh
+
+fi
+
+read -p "Password: " pwd
+
+if [ $pwd = "productionsCx12" ]
+then
+
+   figlet -f standard Sukses! | lolcat
+
+sleep 1
+
+else
+
+    figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh Loader.sh
+
+fi
+
+echo $b "Selamat anda Menggunakan Tools Ini!! :)"
+
+sleep 1
+
+clear
+
+#Process Tools
+
+sh Loader1.sh
+   figlet -f standard Benar! | lolcat
+
+sleep 1
+
+else
+
+    figlet -f slant Salah! | lolcat
+
+sleep 2
+
+sh Loader.sh
+
+fi
+
+echo $b "Selamat Menggunakan Tools Ini!! :)"
+
+sleep 1
+
+clear
+
+#Process Tools
+
+sh Loader1.sh
