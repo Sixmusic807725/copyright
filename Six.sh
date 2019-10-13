4b39c4906236dd99cdd79a73c952976e3cc16f97
Sixmusic807725 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-13 22:57:42 +0700

    Six.sh
    Lah

---
+++ Six.sh
@@ -0,0 +1,471 @@
+#!/bin/bash
+#Process Tools
+clear
+
+echo -e "######################################" | lolcat
+echo -e "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo -e "######################################" | lolcat
+
+sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+    Mr.Six
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+
+sleep 2
+
+echo $yellow "+===========(MR.Six)===========+"
+echo $green "|+TOOLS  : ProgramV1              |"
+echo $green  "|+CODING : MR.Six         |"
+echo $yellow "|+CONTAC : 081227443846.       |"
+echo $yellow "|+THANKS TO : [•]RevoersTeam[•]       |"
+echo $yellow "|+Instagram : maxusgaming   |"
+echo $yellow "|+Ngrecode Besok kesialan datang aminn |"
+echo $yellow "|~Special Thanks to Allah SWT  |"
+echo $yellow "+==========[RevoersTeam]===========+"
+echo $blue ">>=========={MENU}==========<<"
+echo ""
+echo ""
+
+sleep 1
+
+echo $cy"Berikut Adalah Beberapa Tools Yang Saya Kumpulkan"
+
+sleep 1
+
+echo $g"==========================" | lolcat
+echo   "1.DDOS WEBSITE TERPROTEKSI" | lolcat
+echo $g"==========================" | lolcat
+sleep 0.1
+echo ""
+echo $g"==========================" | lolcat
+echo   "2.Hack Fb Beragam Metode" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+echo ""
+
+echo $g"==========================" | lolcat
+echo   "3.Buat Script Deface Sendiri" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+
+sleep 0.1
+
+echo $g"==========================" | lolcat
+echo   "4.Install Tools MetaSploit" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+echo ""
+
+echo $g"==========================" | lolcat
+echo   "5.Install NIK Reader Penipu" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+echo ""
+echo $g"==========================" | lolcat
+echo   "6.Tools Dark Fb 😈 Setan" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "7.Google Translate In Termux" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "8.Kumpulan Terminals Terbaru" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "9.Hack Satelit Remote Control" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "10.Admin Login Url Finder" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "11.Pemulung Bitcoin Work" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "12. Nyepam Call Grab" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "13.Bot Auto Like Facebook" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "14.Clonning Yahoo Mati" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "15.Lite Kalkulator Terx" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "16.Kang Spammer Email" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "17.Virtex WhatsApp Maker" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "18.Sadap Wa Real (Metasploit)" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+
+sleep 0.1
+
+echo ""
+echo $g"==========================" | lolcat
+echo   "19.Vanessa Angel DDOS :v" | lolcat
+echo $g"==========================" | lolcat
+echo ""
+
+sleep 0.1
+
+echo $g"==========================" | lolcat
+echo   "00.Keluar/Minggat" | lolcat
+echo $g"==========================" | lolcat
+
+sleep 0.1
+
+
+echo $r"╭─[Select Number Of Tools:]"
+read -p "╰─root@SixTampans~# " pil
+
+if [ $pil = "1" ] || [ $pil = "01" ]
+then
+
+toilet -f standard DDOS -F gay
+
+sleep 1
+
+git clone https://github.com/Bl4ckDr460n/Garuda-DDOS
+
+cd Garuda-DDOS
+
+python3 garuda.py
+
+
+sleep 1
+
+sh Loader1.sh
+fi
+
+if [ $pil = "2" ] || [ $pil = "02" ]
+then
+
+figlet -f slant HackFb | lolcat
+
+sleep 1
+
+git clone https://github.com/WhatTheSemvak/HackFbBanyak
+
+cd HackFbBanyak
+
+sh Banyak.sh
+
+fi
+
+if [ $pil = "3" ] || [ $pil = "03" ]
+then
+
+figlet -f slant Deface | lolcat
+
+git clone https://github.com/MrTamfanX/MrCreateDeface
+
+cd MrCreateDeface
+
+python2 MRCreateDeface.py
+
+fi
+
+
+
+if [ $pil = "4" ] || [ $pil = "04" ]
+then
+
+figlet -f slant MetaSploit | lolcat
+
+echo "Perhatian!! Tools Ini Install Nya Sangat Lama!! Dan Memakan Memori 520Mb Keatas"
+
+sleep 1
+
+echo "Tekan CTRL+Z Untuk membatalkan" | lolcat
+
+sleep 2
+
+pkg install unstable-repo
+
+pkg install metasploit
+
+msfconsole
+
+fi
+
+if [ $pil = "5" ] || [ $pil = "05" ]
+then
+
+figlet -f standard NIKPenipu
+
+sleep 2
+git clone https://github.com/WhatTheSemvak/NikReader
+
+cd NikReader
+
+sh NIKPnp.sh
+
+fi
+
+if [ $pil = "6" ] || [ $pil = "06" ] 
+then
+
+figlet -f standard DarkFb | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/SETAN
+
+cd SETAN
+
+python2 SETAN.py
+
+fi
+
+if [ $pil = "7" ] || [ $pil = "07" ]
+then
+
+figlet -f standard  Translate | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/Translate
+
+cd Translate
+
+sh terj.sh
+
+fi
+
+if [ $pil = "8" ] || [ $pil = "08" ] 
+then
+
+figlet -f standard Terminals | lolcat
+
+sleep 1
+
+git clone https://github.com/pashayogi/TERMINALS/
+
+cd TERMINALS
+
+sh su.sh
+
+fi
+
+
+if [ $pil = "9" ] || [ $pil = "09" ] 
+then
+
+figlet -f standard Satelit | lolcat
+
+sleep 1
+
+echo "Ini Tools Abal Abal Ya Bambank :v"
+
+git clone https://github.com/pashayogi/satelit
+
+cd satelit
+
+sh hack-satelit.sh
+
+fi
+
+if [ $pil = "10" ]
+then
+
+figlet -f standard AdminFinder | lolcat
+
+sleep 1
+
+git clone https://github.com/the-c0d3r/admin-finder
+
+cd admin-finder
+
+python3 admin-finder.py
+
+fi
+
+if [ $pil = "Ga Tau" ]
+then
+
+echo "Terdeteksi! Bocah Nolep:v" | lolcat
+
+sleep 2
+
+sh Loader1.sh
+
+fi
+
+if [ $pil = "11" ]
+then
+
+figlet -f standard Bitcoin | lolcat
+
+sleep 1
+
+git clone https://github.com/Cvar1984/pemulungBTC
+
+cd pemulungBTC
+
+php mulung.php
+
+fi
+
+if [ $pil = "12" ]
+then
+
+figlet -f standard SpamCall | lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/SpamCall
+
+cd SpamCall
+
+php call.php
+
+fi
+
+if [ $pil = "13" ]
+then
+
+figlet -f standard LikeFb | lolcat
+
+sleep 1
+
+git clone https://github.com/FR13ND8/autolike
+
+cd autolike
+
+php autolike.php
+
+fi
+
+if [ $pil = "14" ]
+then
+
+figlet -f standard Clonning
+
+sleep 1
+
+git clone https://github.com/FR13ND8/EmailVuln
+
+cd EmailVuln
+
+python2 vuln.py
+
+fi
+
+if [ $pil = "15" ]
+then
+figlet -f standard Kalkulator
+sleep 1
+git clone https://github.com/karjok/Litor
+cd Litor
+python litor.py
+fi
+
+if [ $pil = "16 ]
+then
+figlet -f standard SpamMail
+sleep 1
+git clone https://github.com/KANG-NEWBIE/SpamMail
+cd SpamMail
+python2 main.py
+fi
+if [ $pil = "17" ]
+then
+figlet -f standard Virtex | lolcat
+sleep 1
+git clone https://github.com/cyber2611/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+if [ $pil = "18" ]
+then
+figlet -f standard WhatSExp | lolcat
+sleep 1
+git clone https://github.com/MiSetya/WhatsExploit/
+cd WhatsExploit
+sh start-ex.sh
+fi
+if [ $pil = "19" ]
+then
+figlet -f standard VA-DDOS
+sleep 1
+git clone https://github.com/MiSetya/VA-DDOS
+cd VA-DDOS
+python3 VA-DDOS.py
+fi
+
+
